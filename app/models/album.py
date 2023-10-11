938d2cf762ee50d397aec67805135096e133101f
JamieCardona <jamie_cardona@yahoo.com>
2023-10-10 23:01:33 -0600

    album song relationship

---
+++ app/models/album.py
@@ -0,0 +1,12 @@
+from .db import db
+
+class Album(db.Model):
+  __tablename__ = 'albums'
+
+  id = db.Column(db.Integer, primary_key=True)
+  user_id = db.Column(db.Integer, nullable=False)
+  album_name = db.Column(db.String(50), nullable=False)
+  thumbnail = db.Column(db.String, nullable=False)
+  release_year = db.Column(db.Integer, nullable=False)
+
+  songs = db.relationship('Song', back_populates='album')
