b4b317b5f7f3342b7f226da4b52bf9e9adf57fe1
JamieCardona <jamie_cardona@yahoo.com>
2023-10-10 23:01:33 -0600

    album song relationship

--- app/models/song.py
+++ app/models/song.py
@@ -5,8 +5,10 @@ class Song(db.Model):
 
   id = db.Column(db.Integer, primary_key=True)
   user_id = db.Column(db.Integer, nullable=False)
-  album_id = db.Column(db.Integer, nullable=False)
+  album_id = db.Column(db.Integer, nullable=False, db.ForeignKey('albums.id'))
   song_name = db.Column(db.String(50), nullable=False)
   seconds = db.Column(db.Integer, nullable=False)
   song_content = db.Column(db.String, unique=True, nullable=False)
   release_year = db.Column(db.Integer, nullable=False)
+
+  album = db.relationship('Album', back_populates='albums')
