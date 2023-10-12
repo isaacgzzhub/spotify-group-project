0723ba29a21c9aaa17407a52ea982d0e50c10c2a
Jeff <jeffyoon48@gmail.com>
2023-10-11 21:39:02 -0700

    user_likes seeder

--- app/seeds/user_likes.py
+++ app/seeds/user_likes.py
@@ -1,33 +1,31 @@
-from app.models import db, Playlist, environment, SCHEMA
+from app.models import db, UserLike, environment, SCHEMA
 from sqlalchemy.sql import text
 
 
 # Adds a demo user, you can add other songs here if you want
-def seed_playlists():
-    playlist1 = Playlist(user_id=1, name='Favorites', public=True)
-    db.session.add(playlist1)
-    playlist2 = Playlist(user_id=2, name='Workout Mix', public=True)
-    db.session.add(playlist2)
-    playlist3 = Playlist(user_id=1, name='Chill Vibes', public=True)
-    db.session.add(playlist3)
-    playlist4 = Playlist(user_id=3, name='Road Trip Jams', public=True)
-    db.session.add(playlist4)
-    playlist5 = Playlist(user_id=2, name='Study Playlist', public=True)
-    db.session.add(playlist5)
-    playlist6 = Playlist(user_id=4, name='Party Anthems', public=True)
-    db.session.add(playlist6)
-    playlist7 = Playlist(user_id=3, name='Relaxation Station', public=True)
-    db.session.add(playlist7)
-    playlist8 = Playlist(user_id=4, name='Late Night Vibes', public=True)
-    db.session.add(playlist8)
-    playlist9 = Playlist(user_id=1, name='Throwback Hits', public=True)
-    db.session.add(playlist9)
-    playlist10 = Playlist(user_id=2, name='Best of 2020', public=True)
-    db.session.add(playlist10)
+def seed_user_likes():
+    user_like1 = UserLike(user_id=1, song_id=1)
+    db.session.add(user_like1)
+    user_like2 = UserLike(user_id=2, song_id=2)
+    db.session.add(user_like2)
+    user_like3 = UserLike(user_id=1, song_id=3)
+    db.session.add(user_like3)
+    user_like4 = UserLike(user_id=3, song_id=4)
+    db.session.add(user_like4)
+    user_like5 = UserLike(user_id=2, song_id=5)
+    db.session.add(user_like5)
+    user_like6 = UserLike(user_id=4, song_id=6)
+    db.session.add(user_like6)
+    user_like7 = UserLike(user_id=3, song_id=7)
+    db.session.add(user_like7)
+    user_like8 = UserLike(user_id=4, song_id=8)
+    db.session.add(user_like8)
+    user_like9 = UserLike(user_id=1, song_id=9)
+    db.session.add(user_like9)
+    user_like10 = UserLike(user_id=2, song_id=10)
+    db.session.add(user_like10)
 
     db.session.commit()
-
-
 
 # Uses a raw SQL query to TRUNCATE or DELETE the songs table. SQLAlchemy doesn't
 # have a built in function to do this. With postgres in production TRUNCATE
@@ -35,10 +33,10 @@ def seed_playlists():
 # incrementing primary key, CASCADE deletes any dependent entities.  With
 # sqlite3 in development you need to instead use DELETE to remove all data and
 # it will reset the primary keys for you as well.
-def undo_playlist():
+def undo_user_likes():
     if environment == "production":
-        db.session.execute(f"TRUNCATE table {SCHEMA}.playlist RESTART IDENTITY CASCADE;")
+        db.session.execute(f"TRUNCATE table {SCHEMA}.user_likes RESTART IDENTITY CASCADE;")
     else:
-        db.session.execute(text("DELETE FROM playlist"))
+        db.session.execute(text("DELETE FROM user_likes"))
 
     db.session.commit()
