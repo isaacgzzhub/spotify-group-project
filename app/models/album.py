8b6ba15001ec0cd0fdea570538d8059f706943e2
Isaac Gzz <isaacgzz.ing@gmail.com>
2023-10-11 20:51:09 -0600

    Models update

--- app/models/album.py
+++ app/models/album.py
@@ -4,9 +4,10 @@ class Album(db.Model):
   __tablename__ = 'albums'
 
   id = db.Column(db.Integer, primary_key=True)
-  user_id = db.Column(db.Integer, nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
   album_name = db.Column(db.String(50), nullable=False)
   thumbnail = db.Column(db.String, nullable=False)
   release_year = db.Column(db.Integer, nullable=False)
 
-  songs = db.relationship('Song', back_populates='album')
+  songs = db.relationship('Song', back_populates='albums')
+  user = db.relationship('User', back_populates='albums')
--- app/models/song.py
+++ app/models/song.py
@@ -4,11 +4,12 @@ class Song(db.Model):
   __tablename__ = 'songs'
 
   id = db.Column(db.Integer, primary_key=True)
-  user_id = db.Column(db.Integer, nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
   album_id = db.Column(db.Integer, db.ForeignKey("albums.id"), nullable=False)
   song_name = db.Column(db.String(50), nullable=False)
   seconds = db.Column(db.Integer, nullable=False)
   song_content = db.Column(db.String, unique=True, nullable=False)
   release_year = db.Column(db.Integer, nullable=False)
 
-  album = db.relationship('Album', back_populates='albums')
+  album = db.relationship('Album', back_populates='songs')
+  user = db.relationship('User', back_populates='songs')
