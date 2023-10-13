b74989afb7ec7243a78073f80b3f60691dcade6e
nharwit <nharwit@gmail.com>
2023-10-12 22:36:33 -0600

    added add_prefix_for_prod to album model

--- app/models/album.py
+++ app/models/album.py
@@ -4,7 +4,7 @@ class Album(db.Model):
   __tablename__ = 'albums'
 
   id = db.Column(db.Integer, primary_key=True)
-  user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+  user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
   album_name = db.Column(db.String(50), nullable=False)
   thumbnail = db.Column(db.String, nullable=False)
   release_year = db.Column(db.Integer, nullable=False)
