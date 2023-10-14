fd4122929c3b2739a90e08e25691a0230b4c83b7
nharwit <nharwit@gmail.com>
2023-10-14 11:34:23 -0600

    add environment production schema boilerplate for render to all models

--- app/models/song.py
+++ app/models/song.py
@@ -1,8 +1,10 @@
-from .db import db, add_prefix_for_prod
+from .db import db, environment, SCHEMA, add_prefix_for_prod
 
 class Song(db.Model):
   __tablename__ = 'songs'
 
+  if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
 
   id = db.Column(db.Integer, primary_key=True)
   user_id = db.Column(db.Integer, db.ForeignKey(add_prefix_for_prod("users.id")), nullable=False)
