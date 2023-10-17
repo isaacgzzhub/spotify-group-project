89334030ff4f69be801e82701e28f61df1aacce7
Isaac Gzz <isaacgzz.ing@gmail.com>
2023-10-16 22:57:43 -0600

    Added validators

--- app/forms/album_form.py
+++ app/forms/album_form.py
@@ -1,10 +1,10 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, IntegerField
-from wtforms.validators import DataRequired
+from wtforms.validators import DataRequired, Length, URL
 from app.models import Album
 
 class AlbumForm(FlaskForm):
     user_id = IntegerField('user_id', validators=[DataRequired()])
-    album_name = StringField('album_name', validators=[DataRequired()])
-    thumbnail_url = StringField('thumbnail_url', validators=[DataRequired()])
+    album_name = StringField('album_name', validators=[DataRequired(), Length(min=3, max=50)])
+    thumbnail_url = StringField('thumbnail_url', validators=[DataRequired(), URL(require_tld=False)])
     release_year = IntegerField('release_year', validators=[DataRequired()])
