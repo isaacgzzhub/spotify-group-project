4e51526f91553ea4471815a9ed8f738f34e7427f
legendaryyy14 <jamiecardona96@gmail.com>
2023-10-17 22:31:34 -0600

    album release year validator

--- app/forms/album_edit_form.py
+++ app/forms/album_edit_form.py
@@ -6,4 +6,4 @@ from app.models import Album
 class AlbumEditForm(FlaskForm):
     album_name = StringField('album_name', validators=[DataRequired(), Length(min=3, max=50)])
     thumbnail_url = StringField('thumbnail_url', validators=[DataRequired(), URL(require_tld=False)])
-    release_year = IntegerField('release_year', validators=[DataRequired()])
+    release_year = IntegerField('release_year', validators=[DataRequired(message="This field is required and must be an integer")])
--- app/forms/album_form.py
+++ app/forms/album_form.py
@@ -7,4 +7,4 @@ class AlbumForm(FlaskForm):
     user_id = IntegerField('user_id', validators=[DataRequired()])
     album_name = StringField('album_name', validators=[DataRequired(), Length(min=3, max=50)])
     thumbnail_url = StringField('thumbnail_url', validators=[DataRequired(), URL(require_tld=False)])
-    release_year = IntegerField('release_year', validators=[DataRequired()])
+    release_year = IntegerField('release_year', validators=[DataRequired(message="This field is required and must be an integer")])
