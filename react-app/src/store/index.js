2ef6933a99b14ab232d32b8d7b393bd7cd96a9eb
Jeff <jeffyoon48@gmail.com>
2023-10-17 21:58:58 -0700

    started redux store for songs

--- react-app/src/store/index.js
+++ react-app/src/store/index.js
@@ -1,9 +1,13 @@
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import thunk from 'redux-thunk';
 import session from './session'
+import song from './song'
+import albums from './albums'
 
 const rootReducer = combineReducers({
   session,
+  song,
+  albums
 });
 
 
