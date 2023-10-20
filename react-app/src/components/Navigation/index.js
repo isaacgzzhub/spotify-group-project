141b8f4a4d6d3d458a7c3afde37fe49b9eee96b1
nharwit <nharwit@gmail.com>
2023-10-19 20:48:23 -0600

    navbar hugs left, with a lighter grey background, nav links are styled decently, and added the nunito font from google fonts

--- react-app/src/components/Navigation/index.js
+++ react-app/src/components/Navigation/index.js
@@ -1,57 +1,38 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
-import { useSelector } from 'react-redux';
-import ProfileButton from './ProfileButton';
-import './Navigation.css';
+import React from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import ProfileButton from "./ProfileButton";
+import "./Navigation.css";
 
-function Navigation({ isLoaded }){
-	const sessionUser = useSelector(state => state.session.user);
+function Navigation({ isLoaded }) {
+  const sessionUser = useSelector((state) => state.session.user);
 
-	return (
-		<div>
+  return (
+    <div className="navbar-container">
+      <div id="home-profile">
+        {isLoaded && (
+          <div>
+            <ProfileButton user={sessionUser} />
+          </div>
+        )}
+        <div>
+          <NavLink exact to="/">
+            Home
+          </NavLink>
+        </div>
+      </div>
 
-		<div id="home-profile">
-			{isLoaded && (
-				<div>
-					<ProfileButton user={sessionUser} />
-				</div>
-			)}
-			<div>
-				<NavLink exact to="/">Home</NavLink>
-			</div>
-		</div>
-
-		<div id="nav-links">
-			<NavLink to='/songs'>
-				All Songs
-			</NavLink>
-			<NavLink to='/mysongs'>
-				Your Songs
-			</NavLink>
-			<NavLink to='/likedsongs'>
-				Liked Songs
-			</NavLink>
-			<NavLink to='/albums'>
-				All Albums
-			</NavLink>
-			<NavLink to='/myalbums'>
-				Your Albums
-			</NavLink>
-			<NavLink to='/playlists'>
-				All Playlists
-			</NavLink>
-			<NavLink to='/myplaylists'>
-				Your Playlists
-			</NavLink>
-		</div>
-
-
-
-		</div>
-
-
-
-	);
+      <div id="nav-links">
+        <NavLink to="/songs">All Songs</NavLink>
+        <NavLink to="/mysongs">Your Songs</NavLink>
+        <NavLink to="/likedsongs">Liked Songs</NavLink>
+        <NavLink to="/albums">All Albums</NavLink>
+        <NavLink to="/myalbums">Your Albums</NavLink>
+        <NavLink to="/playlists">All Playlists</NavLink>
+        <NavLink to="/myplaylists">Your Playlists</NavLink>
+      </div>
+    </div>
+  );
 }
 
 export default Navigation;
