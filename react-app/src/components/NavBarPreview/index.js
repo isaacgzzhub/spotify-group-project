dc42787a3154a6f11f1e682ca31fde21b2e72eca
nharwit <nharwit@gmail.com>
2023-10-19 21:57:38 -0600

    all albums seems to show the thumbnail urls when we click on that navlink, and also not show any other thumbnails when we click the other navlinks

--- react-app/src/components/NavBarPreview/index.js
+++ react-app/src/components/NavBarPreview/index.js
@@ -1,9 +1,34 @@
+import React, { useEffect } from "react";
+import { NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+
 function NavBarPreview({ activeSection }) {
+  const albumsObj = useSelector((state) => state.albums);
+  const albums = Object.values(albumsObj);
+  console.log(albums);
+
   return (
     <div>
-      {activeSection === "allSongs" && (
+      {activeSection === "allAlbums" && (
         <div>
-          {/* Show the first 5 songs here using map? and slice of state */}
+          <div className="album-tile-list">
+            {/* Show the first five albums here */}
+            {albums.slice(0, 5).map((album) => (
+              <NavLink
+                key={album.id}
+                className="album-tile"
+                to={`/albums/${album.id}`}
+              >
+                <img
+                  className="album-img"
+                  src={`${album.thumbnail_url}`}
+                  alt="album-cover"
+                  title={`${album.album_name}`}
+                />
+                <a>{`${album.album_name}`}</a>
+              </NavLink>
+            ))}
+          </div>
         </div>
       )}
     </div>
