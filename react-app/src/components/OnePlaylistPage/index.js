50c7c8d04b6f1267a1d27b39b9361835a60077a9
nharwit <nharwit@gmail.com>
2023-10-26 20:59:37 -0600

    styled one playlist page

--- react-app/src/components/OnePlaylistPage/index.js
+++ react-app/src/components/OnePlaylistPage/index.js
@@ -9,10 +9,9 @@ import {
 import { getAllSongsThunk } from "../../store/song";
 import { useHistory } from "react-router-dom";
 
-
 function OnePlaylistPage() {
   const dispatch = useDispatch();
-  const history = useHistory()
+  const history = useHistory();
   const { playlistId } = useParams();
   const userId = useSelector((state) => state.session.user.id);
   const playlist = useSelector((state) => state.playlists.currentPlaylist);
@@ -36,36 +35,40 @@ function OnePlaylistPage() {
   }, [dispatch, playlistId]);
 
   return (
-    <div className="playlist-page">
-      <div className="one-playlist-container">
-        <div id="playlist-top-section">
-          <h1> {playlist?.name} Playlist</h1>
-          {userId === playlist?.user_id && (
-            <button onClick={handleAddSongClick}>Add a Song to this Playlist</button>
-          )}
-        </div>
-        <div className="songs-list">
-          {truePlaylistSongs.map((song) => (
-            <div>
-              <NavLink
-                key={song.id}
-                className="album-tile"
-                to={`/songs/${song.id}`}
-              >
-                <img
-                  className="album-img"
-                  src={`${song?.thumbnail_url}`}
-                  alt="album-cover"
-                  title={`${song?.song_name}`}
-                />
-                <a>{`${song?.song_name}`}</a>
-              </NavLink>
-              <button onClick={() => handleDelete(song.id)}>Delete</button>
-            </div>
-          ))}
-        </div>
+    <div className="albums-page">
+      {" "}
+      {/* Tech debt here with using the same classname, but the name here makes no sense */}
+      {/* <div className="album-wrapper"> */}
+      <div id="playlist-top-section">
+        <h1> {playlist?.name} Playlist</h1>
+        {userId === playlist?.user_id && (
+          <button onClick={handleAddSongClick}>
+            Add a Song to this Playlist
+          </button>
+        )}
+      </div>
+      <div className="album-wrapper">
+        {truePlaylistSongs.map((song) => (
+          <div>
+            <NavLink
+              key={song.id}
+              className="album-tile"
+              to={`/songs/${song.id}`}
+            >
+              <img
+                className="album-img"
+                src={`${song?.thumbnail_url}`}
+                alt="album-cover"
+                title={`${song?.song_name}`}
+              />
+              <a>{`${song?.song_name}`}</a>
+            </NavLink>
+            <button onClick={() => handleDelete(song.id)}>Delete</button>
+          </div>
+        ))}
       </div>
     </div>
+    // </div>
   );
 }
 
