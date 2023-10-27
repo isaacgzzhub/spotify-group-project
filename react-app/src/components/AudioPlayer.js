21a471ee9b1d4ed4cf291a3aaa994aa8ba185521
nharwit <nharwit@gmail.com>
2023-10-26 19:08:07 -0600

    hard coded rach 2 piano suite in as mp3 file, can properly see the audio player and play from it at the bottom of each page

---
+++ react-app/src/components/AudioPlayer.js
@@ -0,0 +1,10 @@
+function MyAudioPlayer() {
+  return (
+    <audio controls>
+      <source src="/Rachmaninoff-2-piano-suite.mp3" type="audio/mp3" />
+      Your browser does not support the audio element.
+    </audio>
+  );
+}
+
+export default MyAudioPlayer;
