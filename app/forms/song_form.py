c1a4ba985efe364361836bc4f61c7470e5a80222
nharwit <nharwit@gmail.com>
2023-12-19 22:30:52 -0700

    updated song form to have allowed extensions and file fields, terminal says upload to AWS was successful, but file size is 0, might be an issue with local environment, testing on render now

--- app/forms/song_form.py
+++ app/forms/song_form.py
@@ -1,13 +1,15 @@
 from flask_wtf import FlaskForm
+from flask_wtf.file import FileField, FileAllowed, FileRequired
 from wtforms import StringField, IntegerField
 from wtforms.validators import DataRequired, Email, ValidationError, Length, URL
 from app.models import Song
+from app.api.aws import ALLOWED_EXTENSIONS
 
 class SongForm(FlaskForm):
     user_id = IntegerField('user_id', validators=[DataRequired()])
     album_id = IntegerField('album_id')
     song_name = StringField('song_name', validators=[DataRequired(), Length(min=3, max=50)])
-    thumbnail_url = StringField('thumbnail_url', validators=[DataRequired(), URL(require_tld=False)])
+    thumbnail_url = FileField("Image File", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))]) # string representing url, REPLACED our thumbnail_url column with this instead
     seconds = IntegerField('seconds')
-    song_url = StringField('song_url', validators=[DataRequired(), URL(require_tld=False)])
+    song_url = FileField("Image File", validators=[FileRequired(), FileAllowed(list(ALLOWED_EXTENSIONS))]) # string representing url, REPLACED our song_url column with this instead
     release_year = IntegerField('release_year', validators=[DataRequired(message="This field is required and must be an integer")])
